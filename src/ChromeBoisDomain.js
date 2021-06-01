dce687367330d6ce1d09d4135141514533102843
Sabrina Jones <sabrina.jones87@gmail.com>
2021-06-01 06:25:45 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -42,6 +42,8 @@ resizeHandler = (event) => {
     return (
       <canvas
         onMouseMove={this.handleMouseMove}
+       onClick={this.toggleHandler}
+       onKeyDown={this.resizeHandler}
         width='900'
         height='600'
         tabIndex="0">
